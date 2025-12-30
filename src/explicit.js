dd278c3b5c9c6c0fa3346ea305e9ab58d4d40bbc
aloopday <aloopday@gmail.com>
2025-12-30 09:32:53 +0800

    add explicit.js and ts

---
+++ src/explicit.js
@@ -0,0 +1,14 @@
+//Explicit type annotations
+// String
+var greeting = "Hello,Charles";
+//Number
+var userCount = 42;
+//Boolean
+var isLoading = true;
+//Array of numbers
+var scores = [18, 98, 99];
+// Outpur the values
+console.log(greeting);
+console.log(userCount);
+console.log(isLoading);
+console.log(scores);
---
+++ src/explicit.ts
@@ -0,0 +1,16 @@
+//Explicit type annotations
+// String
+let greeting: string = "Hello,Charles";
+//Number
+let userCount: number =42;
+
+//Boolean
+let isLoading: boolean=true;
+//Array of numbers
+let scores: number[]=[18,98,99];
+
+// Outpur the values
+console.log(greeting);
+console.log(userCount);
+console.log(isLoading);
+console.log(scores);
