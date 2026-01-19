4ae48abc6dd7f4e5d61c53fe7465a4dc678d854b
aloopday <aloopday@gmail.com>
2026-01-19 16:44:28 +0800

    add record type

---
+++ src/record.js
@@ -0,0 +1,5 @@
+var nameAgeMap = {
+    'Sonia': 29,
+    'Zubair': 31
+};
+console.log(nameAgeMap);
---
+++ src/record.ts
@@ -0,0 +1,6 @@
+
+const nameAgeMap: Record<string,number>={
+    'Sonia':29,
+    'Zubair':31
+};
+console.log(nameAgeMap);
